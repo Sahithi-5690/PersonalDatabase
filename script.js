6f1af9074452b94c2373baf3827e1122c9db78ae
sony-valeti <143113877+sony-valeti@users.noreply.github.com>
2024-05-15 11:27:00 -0500

    sony : Tracker Page Updated

--- script.js
+++ script.js
@@ -43,3 +43,18 @@ function logout() {
 
 // Add an event listener to the logout button
 document.getElementById('logout').addEventListener('click', logout);
+
+// function openjob(evt, job) {
+//     var i, tabcontent, tablinks;
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//       tabcontent[i].style.display = "none";
+//     }
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//       tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+//     document.getElementById(job).style.display = "block";
+//     evt.currentTarget.className += " active";
+//   }
+   
