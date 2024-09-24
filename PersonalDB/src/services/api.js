8b09986e0e7f26f85a5f1b4f1e9cb55efd0d672e
Basina Pinakapaani <142944580+Paani143@users.noreply.github.com>
2024-09-23 23:56:16 -0500

    Delete PersonalDB directory

--- PersonalDB/src/services/api.js
+++
@@ -1,9 +0,0 @@
-// src/services/api.js
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:8081', // Base URL of your backend server
-  timeout: 10000, // Optional: timeout in milliseconds
-});
-
-export default api;
